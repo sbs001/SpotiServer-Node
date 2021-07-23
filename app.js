2f9f5a688c0ee7b27f8512c39978c7eec94c95a4
Sebastian <tassone_af@yahoo.com>
2021-07-23 16:25:06 -0300

    Add: express config

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const routes = require('./routes/index.js');
+const { urlencoded } = require('body-parser');
+
+require('./db.js');
+
+const server = express();
+
+server.name = 'SPOTI_SERVER';
+
+server.use(bodyParser, urlencoded({ extended: true, limit: '50mb' }));
+server.use(bodyParser.json({ limit: '50mb' }));
+server.use(cookieParser());
+server.use(morgan('dev'));
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-Whit, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    next();
+});
+
+server.use('/', routes);
+
+//Error catching emdware
+server.use((err, req, res, next) => {
+    const status = err.status || 500;
+    const message = err.message || err;
+    console.log(err);
+    res.status(status).send(message);
+});
+
+module.exports = server;
