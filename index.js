72d10c08e17599ec632d35e0852f2e7f127f44b0
Sebastian <tassone_af@yahoo.com>
2021-07-29 16:08:14 -0300

    prepare database for deploy

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const { conn } = require('./db.js');
 
 conn.sync({ force: false })
     .then(() => {
-        server.listen(3001, () => console.log('%s listening at 3001'))
+        server.listen(process.env.PORT, () => console.log('%s listening at ' + process.env.PORT))
     })
